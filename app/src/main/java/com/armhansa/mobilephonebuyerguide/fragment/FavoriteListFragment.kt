565c6f0295838fe18a18b093e292d8e82664475a
Arm Hansa <armhansa@gmail.com>
2019-09-17 09:00:34 +0700

    Create FavoriteListFragment Structure

---
+++ app/src/main/java/com/armhansa/mobilephonebuyerguide/fragment/FavoriteListFragment.kt
@@ -0,0 +1,25 @@
+package com.armhansa.mobilephonebuyerguide.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.armhansa.mobilephonebuyerguide.R
+
+class FavoriteListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FavoriteListFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_list, container, false)
+    }
+
+
+}
