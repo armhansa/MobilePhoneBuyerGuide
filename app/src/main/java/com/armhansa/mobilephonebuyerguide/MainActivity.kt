fe56a2b62eab8b6c3bcdf85f16bdbb027b900750
Arm Hansa <armhansa@gmail.com>
2019-09-17 09:05:40 +0700

    Edit MainActivity and Add 2 Fragment in TabModel

--- app/src/main/java/com/armhansa/mobilephonebuyerguide/MainActivity.kt
+++ app/src/main/java/com/armhansa/mobilephonebuyerguide/MainActivity.kt
@@ -2,6 +2,9 @@ package com.armhansa.mobilephonebuyerguide
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.armhansa.mobilephonebuyerguide.fragment.FavoriteListFragment
+import com.armhansa.mobilephonebuyerguide.fragment.PhoneListFragment
+import com.armhansa.mobilephonebuyerguide.model.TabModel
 import com.armhansa.mobilephonebuyerguide.ui.main.SectionsPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,10 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+
+        val tabModel = arrayListOf(
+            TabModel("Mobile List", PhoneListFragment.newInstance()),
+            TabModel("Favorite List", FavoriteListFragment.newInstance()))
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(tabModel, supportFragmentManager)
         viewPager.adapter = sectionsPagerAdapter
         tabs.setupWithViewPager(viewPager)
-
     }
+
 }
