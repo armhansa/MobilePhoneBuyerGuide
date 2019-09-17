9467fce956ffc56c0e656b942572bdb8967370eb
Arm Hansa <armhansa@gmail.com>
2019-09-17 16:23:18 +0700

    Add parameter id into PhoneApiService

--- app/src/main/java/com/armhansa/mobilephonebuyerguide/service/PhoneApiService.kt
+++ app/src/main/java/com/armhansa/mobilephonebuyerguide/service/PhoneApiService.kt
@@ -4,11 +4,12 @@ import com.armhansa.mobilephonebuyerguide.entity.PhoneEntity
 import com.armhansa.mobilephonebuyerguide.entity.PhoneImageEntity
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PhoneApiService {
     @GET("mobiles/")
     fun getMobiles(): Call<List<PhoneEntity>>
 
-    @GET("mobiles/1/images/")
-    fun getMobileImgById(): Call<PhoneImageEntity>
+    @GET("mobiles/{id}/images/")
+    fun getMobileImgById(@Path("id") phoneId: Int): Call<List<PhoneImageEntity>>
 }
